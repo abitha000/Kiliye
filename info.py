44aabe59d43a35ad1c3b6f4a82e8da8871fab60b
abitha000 <abithak000@yahoo.com>
2024-08-28 23:33:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://t.me/+CCmc9nVd8JViNTNl")
+URL = environ.get("URL", "https://tasteless-costanza-kalaisel-bf0099ca.koyeb.app/")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
