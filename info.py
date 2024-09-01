8a5201cb18ac10b2b587f5934beb6ed719ceac54
abitha000 <abithak000@yahoo.com>
2024-09-01 22:26:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,17 +17,17 @@ def is_valid_ip(ip):
     return re.match(ip_pattern, ip) is not None
 
 # Bot information
-API_ID = environ.get('API_ID', '24401235')
+API_ID = environ.get('API_ID', '24143648')
 if len(API_ID) == 0:
     print('Error - API_ID is missing, exiting now')
     exit()
 else:
     API_ID = int(API_ID)
-API_HASH = environ.get('API_HASH', '149f7e13d7d861b27cffc3ab1fd52b22')
+API_HASH = environ.get('API_HASH', 'e0a048e72723774003ae2e3ac5927e97')
 if len(API_HASH) == 0:
     print('Error - API_HASH is missing, exiting now')
     exit()
-BOT_TOKEN = environ.get('BOT_TOKEN', '7286009662:AAGAJnoBHXPYOJQ3wz6po-RU-zNBteaBQgE')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6491531689:AAGmxmlUNr13DsdEH7uV3q9iXjmbmcMlmZw')
 if len(BOT_TOKEN) == 0:
     print('Error - BOT_TOKEN is missing, exiting now')
     exit()
@@ -37,7 +37,7 @@ PORT = int(environ.get('PORT', '80'))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/58fef5cb458d5b29b0186.jpg https://telegra.ph/file/f0aa4f433132769f8970c.jpg https://telegra.ph/file/f515fbc2084592eca60a5.jpg https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg https://telegra.ph/file/6045ba953af4def846238.jpg')).split()
 
 # Bot Admins
-ADMINS = environ.get('ADMINS', '7255612720')
+ADMINS = environ.get('ADMINS', '1556830659')
 if len(ADMINS) == 0:
     print('Error - ADMINS is missing, exiting now')
     exit()
@@ -48,7 +48,7 @@ else:
 INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1002237503909').split()]
 if len(INDEX_CHANNELS) == 0:
     print('Info - INDEX_CHANNELS is empty')
-LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002183761611')
+LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002214772109')
 if len(LOG_CHANNEL) == 0:
     print('Error - LOG_CHANNEL is missing, exiting now')
     exit()
@@ -73,7 +73,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
 SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/universe_we_are')
-OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/Quin_sarah")
+OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/lochapochak")
 UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/we_are_universee')
 FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/Scarlett_request')
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/we_are_universee")
@@ -113,13 +113,13 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'sampleupi@upi')
 
 # for stream
 IS_STREAM = is_enabled('IS_STREAM', True)
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002178415640")
+BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002038146800")
 if len(BIN_CHANNEL) == 0:
     print('Error - BIN_CHANNEL is missing, exiting now')
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://tasteless-costanza-kalaisel-bf0099ca.koyeb.app/")
+URL = environ.get("URL", "https://t.me/+Gq7I6A_28TE3Y2I1")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
